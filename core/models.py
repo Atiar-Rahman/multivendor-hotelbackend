315cabe00615497da3fcd4edeb408a25017a6c69
Atiar-Rahman <atiar1709@gmail.com>
2026-04-08 09:47:06 +0600

    softdeletemodel create and manager customization

--- core/models.py
+++ core/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+# full softdelete and abstract model
+
+User = get_user_model()
+
+#custom manager create
+
+class SoftDeleteManager(models.Manager):
+    """Return only active(not soft deleted) objects by default"""
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted = False)
+    
+
+class AllObjectsManager(models.Manager):
+    """Return all objects including soft deleted"""
+    def get_queryset(self):
+        return super().get_queryset()
+
+class DeleteObjectsManager(models.Manager):
+    """Return all objects including soft deleted"""
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=True)
+
+
+
+# Abstract Base Model
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(default=True)
+    deleted_at = models.DateTimeField(null=True,blank=True)
+    deleted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="deleted_%(class)ss")
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # managers
+    objects = AllObjectsManager() # default: all objects
+    active_objects = SoftDeleteManager() # not delete
+    deleted_objects = DeleteObjectsManager() # only deleted
+
+
+    class Meta:
+        abstract = True
+
+    
+    # soft delete methods
+
+    def soft_delete(self, user=None):
+        """Mark objects as deleted"""
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+
+        if user:
+            self.deleted_by = user
+
+        self.save()
+
+
+    def restore(self):
+        """Restore a soft deleted objects"""
+        self.is_deleted = False
+        self.deleted_at = None  
+        self.deleted_by = None
+        self.save()
+    
+    
